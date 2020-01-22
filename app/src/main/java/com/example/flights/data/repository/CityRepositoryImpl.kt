305b0b7bfa6164ac130feebd49ad53ac5f54d82c
Anton Domnikov <anton.domnikov@touchin.ru>
2020-01-22 09:04:46 +0300

    fixed hardcoded locale

--- app/src/main/java/com/example/flights/data/repository/CityRepositoryImpl.kt
+++ app/src/main/java/com/example/flights/data/repository/CityRepositoryImpl.kt
@@ -3,13 +3,14 @@ package com.example.flights.data.repository
 import com.example.flights.domain.repository.CityRepository
 import com.example.flights.data.network.api.CitiesApi
 import com.example.flights.data.network.response.CitiesResponse
+import java.util.Locale
 import javax.inject.Inject
 
 class CityRepositoryImpl @Inject constructor(
     private val citiesApi: CitiesApi
 ) : CityRepository {
 
-    override fun getCities(query: String) = citiesApi.getCities(query)
+    override fun getCities(query: String) = citiesApi.getCities(query, Locale.getDefault().language)
         .map(CitiesResponse::cities)
 
 }
